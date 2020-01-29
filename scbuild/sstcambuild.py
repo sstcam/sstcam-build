c050693ef845dcbf381f619be0c97df6e6aef7be
Samuel <samuel.d.flis@gmail.com>
2020-01-29 12:04:13 +0100

    A small update to the build system, the application now uses subparsers and it's possible to chose whether to use ssh when cloning the repos

--- scbuild/sstcambuild.py
+++ scbuild/sstcambuild.py
@@ -2,12 +2,12 @@ import argparse
 import subprocess
 import os
 import yaml
-from shutil import copyfile,copytree
+from shutil import copyfile, copytree
 
-sub_projects = {'sstcam-base': 'https://github.com/sstcam/sstcam-base.git',
-                'sstcam-template': 'https://github.com/sflis/sstcam-template.git',
-                'sstcam-common': "https://github.com/sstcam/sstcam-common.git"}
-
+sub_projects = {'sstcam-base': 'github.com/sstcam/sstcam-base.git',
+                'sstcam-template': 'github.com/sflis/sstcam-template.git',
+                'sstcam-common': "github.com/sstcam/sstcam-common.git"}
+mode = {'https': "https://", 'ssh': "ssh://git@"}
 dependencies = {'pybind11': "https://github.com/pybind/pybind11.git"}
 
 
@@ -19,43 +19,54 @@ def create_dir(dir_name):
         print("Directory ", dir_name, " already exists")
 
 
+def init(args):
+    currentDirectory = os.getcwd()
+    root_path = currentDirectory
+    build_descr_file = os.path.join(root_path, ".sstcam-buildconfig.yaml")
+    if os.path.exists(build_descr_file) and not args.force:
+        print("This build is already initialized! To update use the update command instead")
+        exit()
+    build_descr = {}
+
+    sel_mode = "https"  # mode[]
+    if args.ssh:
+        sel_mode = "ssh"  # mode["ssh"]
+    build_descr['git_mode'] = sel_mode
+
+    with open(build_descr_file, 'w') as f:
+        yaml.dump(build_descr, f)
+
+    sstcam_build_system_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    template_dir = os.path.join(sstcam_build_system_dir, 'root_template')
+    create_dir(os.path.join(root_path, 'build'))
+
+    for sp, git_url in sub_projects.items():
+        print(f"{mode[sel_mode]}{git_url}")
+        print("Cloning project {}".format(sp))
+        subprocess.run(['git', 'clone', f"{mode[sel_mode]}{git_url}"])
+    copyfile(os.path.join(template_dir, 'CMakeLists.txt'),
+            os.path.join(root_path, 'CMakeLists.txt'))
+    copytree(os.path.join(template_dir, 'python'),
+            os.path.join(root_path, 'python'))
+
+
 def main():
 
     parser = argparse.ArgumentParser(
         description="SST Camera build system",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
-    parser.add_argument("init", help="Initialize the build")
+    subparsers = parser.add_subparsers()
+    init_parser = subparsers.add_parser('init', help="Initialize the build")
+    init_parser.set_defaults(func=init)
+    init_parser.add_argument("-s, --ssh", dest='ssh', action='store_true', help="Use ssh key authorization with Github")
+    init_parser.add_argument("-f, --force", dest='force', action='store_true', help="Force command")
+
     args = parser.parse_args()
-    if args.init:
-        currentDirectory = os.getcwd()
-        root_path = currentDirectory
-        with open(os.path.join(root_path, ".sstcam-buildconfig.yaml"), 'w') as f:
-            f.write(" ")
-
-        sstcam_build_system_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-        template_dir = os.path.join(sstcam_build_system_dir,'root_template')
-        create_dir(os.path.join(root_path, 'build'))
-        for sp, git_url in sub_projects.items():
-            print("Cloning project {}".format(sp))
-            subprocess.run(['git', 'clone', git_url])
-        copyfile(os.path.join(template_dir, 'CMakeLists.txt'),
-                os.path.join(root_path, 'CMakeLists.txt'))
-        copytree(os.path.join(template_dir,'python'),
-                os.path.join(root_path,'python'))
-
-        # deps_dir = os.path.join(root_path, 'deps')
-        # create_dir(os.path.join(deps_dir, "build"))
-
-        # for dp, git_url in dependencies.items():
-        #     print("Cloning dependency {}".format(dp))
-        #     subprocess.run(['git', 'clone', git_url], cwd=os.path.join(root_path, "deps"))
-        #     print("Installing dependency {}".format(dp))
-        #     subprocess.run(['cmake', '../{}'.format(dp), "-DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX"],
-        #                         cwd=os.path.join(deps_dir, "build"))
-        #     subprocess.run(['make', "install"], cwd=os.path.join(deps_dir, "build"))
+    args.func(args)
 
 
 if __name__ == '__main__':
     main()
+
 
